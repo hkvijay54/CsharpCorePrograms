b42f062aa7fad84d20e8123d81f344b8e8f10c80
hkvijay54 <hkvijay54@gmail.com>
2021-12-18 16:23:05 +0530

    Program to Compute Quotient and Remainder

--- Day5AssginmentProrograms/Program.cs
+++ Day5AssginmentProrograms/Program.cs
@@ -1,31 +1,17 @@
 ﻿using System;
-// prime factorisation.
+// Program to Compute Quotient and Remainder.
 class Program
 {
-    public static void PrimeFact(int n)
-    {
-        while (n % 2 == 0)
-        {
-            Console.Write(2 + " ");
-            n /= 2;
-        }
-        for (int i = 3; i <= Math.Sqrt(n); i += 2)
-        {
-            while(n%i == 0)
-            {
-                Console.Write(i + " ");
-                n /= i;
-            }
-        }
-        if(n>2)
-        {
-            Console.Write(n);
-        }
-    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number");
-        int n = Convert.ToInt32(Console.ReadLine());
-        PrimeFact(n);
+        Console.WriteLine("Enter a Divident");
+        int dividend = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a Divisor");
+        int divisor = Convert.ToInt32(Console.ReadLine());
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        Console.WriteLine("Quotient = " + quotient);
+        Console.WriteLine("Remainder = " + remainder);
     }
 }
