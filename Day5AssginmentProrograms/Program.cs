df0d3a5fd4149f29ba4f8c96334de919037f0979
hkvijay54 <hkvijay54@gmail.com>
2021-12-18 15:40:56 +0530

    Power of 2

--- Day5AssginmentProrograms/Program.cs
+++ Day5AssginmentProrograms/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-// Flip Coin and print percentage of Heads and Tails.
+// Power of 2.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter Year : ");  
-            int Year = Convert.ToInt32(Console.ReadLine());
-        if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0)) Console.WriteLine("{0} is a Leap Year.", Year);  
-            else Console.WriteLine("{0} is not a Leap Year.", Year); 
+        Console.WriteLine("Enter a number : ");  
+        int num = Convert.ToInt32(Console.ReadLine());
+        double res;
+        for(int i=0;i<=num;i++)
+        {
+            res = Math.Pow(2, i);
+            Console.WriteLine("2^" + i + "= " + res);
+        }
     }
 }
