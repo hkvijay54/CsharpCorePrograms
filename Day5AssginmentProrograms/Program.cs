338d2370e2f5a41eb96c829f0254fb769445ad1c
hkvijay54 <hkvijay54@gmail.com>
2021-12-18 16:30:05 +0530

    Program to swap two numbers

--- Day5AssginmentProrograms/Program.cs
+++ Day5AssginmentProrograms/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
-// Program to Compute Quotient and Remainder.
+// Program to Swap Two Numbers.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a Divident");
-        int dividend = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter a Divisor");
-        int divisor = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a first number");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a Second Number");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        int temp;
 
-        int quotient = dividend / divisor;
-        int remainder = dividend % divisor;
-        Console.WriteLine("Quotient = " + quotient);
-        Console.WriteLine("Remainder = " + remainder);
+        Console.WriteLine("Before swapping");
+        Console.WriteLine("first number: "+num1+" second number: "+num2);
+
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        Console.WriteLine("After swapping");
+        Console.WriteLine("first number: " + num1 + " second number: " + num2);
     }
 }
